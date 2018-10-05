e50f7a252d8a0fc5d5e4b8bf7ec43e8bb1ab5eaf
Matyas Matyas <duplamatyi@gmail.com>
2018-10-05 22:18:41 +0200

    Removed extra comments

--- 17-DynamicProgramming/MinAbsNum.java
+++ 17-DynamicProgramming/MinAbsNum.java
@@ -1,9 +1,3 @@
-// you can also use imports, for example:
-// import java.util.*;
-
-// you can write to stdout for debugging purposes, e.g.
-// System.out.println("this is a debug message");
-
 //https://app.codility.com/programmers/lessons/17-dynamic_programming/min_abs_sum/
 // you can also use imports, for example:
 // import java.util.*;
